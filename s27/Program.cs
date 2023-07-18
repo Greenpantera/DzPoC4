2c7ce184b4d76f5718b854348f657abe7a7ea35e
Ekaterina <greenpantera@rambler.ru>
2023-07-18 18:10:44 +0500

    Решение задач

---
+++ s27/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetSum(int limit)
+{
+    int sum=0;
+    while(limit>0)
+    {
+       int res;
+       limit = Math.DivRem(limit, 10, out res);
+       sum+=res;
+    }
+    return sum;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите число А: ");
+Console.WriteLine($"Сумма чисел в числе {num} равна {GetSum(num)}");
---
+++ s29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[]MakeNewBinaryArray(int size)
+{
+    int[]array=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        array[i] = new Random().Next();   
+     }
+     return array;
+}
+
+int[]array = MakeNewBinaryArray(8);
+Console.WriteLine(string.Join(",", array)); 
